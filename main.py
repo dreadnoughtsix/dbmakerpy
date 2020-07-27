0b627ecab143c92b76363d8f27e2286e7473f440
Clarenz James Caba <clarenz.caba@gmail.com>
2020-07-27 19:05:08 -0400

    Make a simple database lister and prompt to access database
    When the current working directory already has a database file(s),
    script should prompt user to access a specific database or allow the
    user the list databases.
    
    - Infinitely loop and ask user to access a database to continue
    - Handle user input when listing the database names. This can be
    achieved using "--lsdb" argument
    - Return and continue through the loop if the database name does not
    exist
    - Access the database when the database name is found
    
    Resolves issue #4

--- main.py
+++ main.py
@@ -18,7 +18,31 @@ def main():
         # Loop through the directory and print out the names of all the databases
         for file in os.listdir('.'):
             if file.endswith('.db'):
+
+                # Append the name of the file into the databases list
+                databases.append(file)
                 print(file)
+
+        while (1):
+            user_input = input("\nWhich database would you like to access (enter '--lsdb' for database list): ")
+            
+            # Returns a list of all the database names in the directory
+            if user_input == "--lsdb":
+                print("\n")
+                for database in databases:
+                    print(database)
+
+            # File not found. Print warning and continue the loop
+            elif user_input not in databases:
+                print("That database does not exist...")
+                continue
+
+            # File is found. Print prompt and exit the loop
+            else:
+                print("Accessing {}".format(user_input))
+                break
+
+
 
     # No database files are found in the directory
     # Ask the user if they would like to start one
@@ -29,7 +53,7 @@ def main():
             database_name = input("Please enter the new database name: ")
 
             # If the database_name does not have an extension, add the .db extension
-            if database_name.find(DB_EXTENSION) == -1: # the string used does not have the database extension
+            if database_name.find(DB_EXTENSION) == -1:
                 database_name += DB_EXTENSION
             
             print("The database name is:", database_name)
